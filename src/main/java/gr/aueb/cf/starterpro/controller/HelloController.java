bff48892f07caf5102697831510b6578f69a2a04
a8anassis <a8anassis@gmail.com>
2026-07-14 12:55:06 +0300

    feat(controller): add welcome endpoint to greet users by name

--- src/main/java/gr/aueb/cf/starterpro/controller/HelloController.java
+++ src/main/java/gr/aueb/cf/starterpro/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/starter")
@@ -13,5 +14,12 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("message", "Hello World!");
         return "index";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(@RequestParam(name = "username", defaultValue = "Guest") String givenName,
+                          Model model) {
+        model.addAttribute("name", givenName);
+        return "welcome";
     }
 }
